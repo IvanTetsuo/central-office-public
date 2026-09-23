c2a5ae37d17b82005b37227b8a2b427bdb61d0b6
IvanTetsuo <ivan.d2413@gmail.com>
2026-09-23 17:19:47 +0300

    сделана проверка авторизации shop

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -3,33 +3,38 @@ import { ShopService } from './shop.service';
 import { CreateShopDto } from './dto/create-shop.dto';
 import { UpdateShopDto } from './dto/update-shop.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ShopJwtGuard } from '../shop-auth/shop-jwt.guard';
 
 @Controller('shop')
-@UseGuards(JwtAuthGuard)
 export class ShopController {
   constructor(private readonly shopService: ShopService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createShopDto: CreateShopDto) {
     return this.shopService.create(createShopDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.shopService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(ShopJwtGuard)
   findOne(@Param('id') id: string) {
     return this.shopService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(ShopJwtGuard)
   update(@Param('id') id: string, @Body() updateShopDto: UpdateShopDto) {
     return this.shopService.update(id, updateShopDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.shopService.remove(id);
   }
